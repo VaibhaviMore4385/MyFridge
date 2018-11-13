f6c8e52cfc3ff9f3cf517f38359be59a1b342a07
Vaibhavi Milind More <Vmore4385@conestogac.on.ca>
2018-11-12 19:30:54 -0500

    project connected with database

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
-
 var router = express.Router();
 
+var ctrlMain = require('../controllers/foodItems');
+var ctrlItemList = require('../controllers/updateFoodList');
+var ctrlAddEdit = require('../controllers/food_list');
 
-
-var ctrlMain = require('../controllers/main');
-/* GET home page. */
 router.get('/', ctrlMain.index);
+router.get('/updateFoodList', ctrlItemList.foodList);
+router.get('/food_list', ctrlAddEdit.foodCreate);
+router.post('/food_list', ctrlAddEdit.doFoodCreate);
 
 
 module.exports = router;
